1ac379f0def23dee372b021c563c0d4b169628ba
dojo24 <mlongenberger@codingdojo.com>
2021-03-16 17:06:59 -0400

    added key

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-    $.get("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY", function(res) {
+    $.get("https://api.nasa.gov/planetary/apod?api_key=1aRkoVUWw3h7ZXmnd9OSWG2MtJUuQW2N6YYUgpat", function(res) {
         var html_str = "";
         html_str += "<img src='" + res.url + "' alt='charcater img' />";
         $(".photo").html(html_str)
