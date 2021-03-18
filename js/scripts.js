3089d6fbcfca881810652eac21730a75936abec7
dojo24 <mlongenberger@codingdojo.com>
2021-03-18 17:23:00 -0400

    added form and user input

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,26 @@
 $(document).ready(function(){
+    var apiKey = '8ab82243dc2b9fe4c8e68d061a14e1c4'
+    // var apiKey = '1a030624be57d904c2ea834c553e9c4c'
+
+
+    $('form').submit(function () {
+        var city = $('#city').val();
+        var cityString = `${city}`
+
+        var url = `http://api.openweathermap.org/data/2.5/weather?q=${cityString}&appid=${apiKey}&units=imperial`
+
+        $.get(url, function (res) {
+            console.log(res)
+            var htmlString = `<h1>${cityString}</h1><p>Temperature: ${res.main.temp}&#8457;</p>`
+            htmlString+= `<p>Description: ${res.weather[0].description}</p><p>Wind Speed: ${res.wind.speed} mph</p>`
+            $('#forecast').html(htmlString);
+
+        }, 'json');
+        return false;
+    });
+
     $.get("https://api.nasa.gov/planetary/apod?api_key=1aRkoVUWw3h7ZXmnd9OSWG2MtJUuQW2N6YYUgpat", function(res) {
+        console.log(res)
         var html_str = "";
         html_str += "<img src='" + res.url + "' alt='charcater img' />";
         $(".photo").html(html_str)
@@ -10,4 +31,6 @@ $(document).ready(function(){
             width: 'toggle'
         })
     })
+
+
 })
